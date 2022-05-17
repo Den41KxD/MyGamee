5c594c80846df66453c8eea92fb2087f1335de95
Den41KxD <den41kxd67@gmail.com>
2022-05-17 18:44:50 +0300

    final

---
+++ game.py
@@ -0,0 +1,235 @@
+import datetime
+import random
+
+from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.uix.textinput import TextInput
+
+
+class MyGameApp(BoxLayout):
+    def __init__(self, size_of_gread):
+        super(MyGameApp, self).__init__()
+        self.size_of_gread = size_of_gread
+
+
+    def build(self):
+        self.main_layout = BoxLayout(orientation="vertical")
+        self.start_time = datetime.datetime.now()
+        self.flag1 = False
+        self.flag2 = False
+        self.button_one = False
+        self.button_two = False
+        self.new_game_flag = False
+        self.info_layout = BoxLayout(orientation="horizontal", size=(800, 50), size_hint=(None, None))
+        self.steps_label = Label(text=str(0))
+        self.clock = Clock.schedule_interval(self.set_time, 1)
+        self.back_button = Button(text='Back', on_press = self.back_button_dis)
+        self.timer_label = Label(text='0')
+        self.info_layout.add_widget(self.back_button)
+        self.info_layout.add_widget(Label(text='Steps: '))
+        self.info_layout.add_widget(self.steps_label)
+        self.info_layout.add_widget(Label(text='Time: '))
+        self.info_layout.add_widget(self.timer_label)
+        self.main_layout.add_widget(self.info_layout)
+        self.all_number_layout = BoxLayout(orientation="vertical")
+        self.main_mass = self.get_matrix()
+
+        row_count = 0
+        for row in self.main_mass:
+            h_layout = BoxLayout()
+            if row_count < self.size_of_gread:
+                lable_count = 0
+                for label in row:
+                    lable_count += 1
+                    if lable_count <= self.size_of_gread:
+                        button = Button(text=str(label), pos_hint={"center_x": 0.5, "center_y": 0.5})
+                        button.bind(on_press=self.on_button_press)
+                    else:
+                        button =TextInput(
+                            text=str(label),
+                            pos_hint={"center_x": 0.5, "center_y": 0.5},
+                            halign = "center",
+                            background_color =[1,0.8,0],
+                            disabled = True
+                        )
+                    h_layout.add_widget(button)
+
+            else:
+                for i in range(self.size_of_gread):
+                    label = row[i]
+                    button =TextInput(
+                            text=str(label),
+                            pos_hint={"center_x": 0.5, "center_y": 0.5},
+                            halign = "center",
+                            background_color =[1,0.8,0],
+                            disabled=True
+                            )
+                    h_layout.add_widget(button)
+                self.end_game_button = Button(
+                    text=str(self.generete_rand),
+                    pos_hint={"center_x": 0.5, "center_y": 0.5},
+                    on_press=self.check_end_game,
+                    background_color=[0.07, 0.9, 0.9, 0.8])
+                h_layout.add_widget(self.end_game_button)
+            self.all_number_layout.add_widget(h_layout)
+            row_count += 1
+
+        self.main_layout.add_widget(self.all_number_layout)
+        return self.main_layout
+
+    def on_button_press(self, button):
+
+        if not self.flag1:
+            self.flag1 = True
+            self.button_one = button
+            self.button_one.background_color = [0.216, 0.69, 0.22]
+        else:
+            self.flag2 = True
+            self.button_two = button
+        if self.flag1 and self.flag2:
+            self.steps_label.text = str(int(self.steps_label.text)+1)
+            tmp_var = self.button_one.text
+            self.button_one.text = self.button_two.text
+            self.button_two.text = tmp_var
+            self.flag2 = False
+            self.flag1 = False
+            self.button_one.background_color = [1, 1, 1]
+            self.button_one.index = self.button_one.parent.children.index(self.button_one)
+            self.button_two.index = self.button_two.parent.children.index(self.button_two)
+
+            new_sum = 0
+            new_sum2 = 0
+
+            for i in range(1, self.size_of_gread + 1):
+                new_sum += int(self.button_one.parent.children[i].text)
+                new_sum2 += int(self.button_two.parent.children[i].text)
+            self.button_one.parent.children[0].text = str(new_sum)
+            self.button_two.parent.children[0].text = str(new_sum2)
+            new_sum_3 = 0
+            new_sum_4 = 0
+            first_flag = False
+            for i in self.all_number_layout.children:
+                if not first_flag:
+                    first_flag = True
+                else:
+                    new_sum_3 += int(i.children[self.button_one.index].text)
+                    new_sum_4 += int(i.children[self.button_two.index].text)
+            self.all_number_layout.children[0].children[int(self.button_one.index)].text = str(new_sum_3)
+            self.all_number_layout.children[0].children[int(self.button_two.index)].text = str(new_sum_4)
+
+    def set_time(self, *args):
+        self.timer_label.text = str(datetime.datetime.now() - self.start_time)[0:7]
+
+    def back_button_dis(self,button):
+        self.main_layout.parent.manager.current = 'settings'
+
+    def check_end_game(self, button):
+
+        if not self.new_game_flag:
+            flag = True
+            for row in self.all_number_layout.children:
+                if int(row.children[0].text) != self.generete_rand:
+                    flag = False
+            for button in self.all_number_layout.children[0].children:
+                if int(button.text) != self.generete_rand:
+                    flag = False
+            if flag:
+                self.clock.cancel()
+                self.end_game_button.background_color = [0, 1, 0]
+                self.end_game_button.text = ' Good\n Tap for New Game'
+                self.new_game_flag = True
+
+                popup_content = Button(text=f'Good job, you result:\n'
+                                            f'passed in {self.timer_label.text} minutes\n'
+                                            f'completed in {self.steps_label.text} steps\n',
+                                       size=(400, 400))
+                popup = Popup(content=popup_content, auto_dismiss=False, title='nice', size_hint=(None, None),
+                              size=(400, 400))
+
+                # bind the on_press event of the button to the dismiss function
+                popup_content.bind(on_press=popup.dismiss)
+                # open the popup
+                popup.open()
+
+
+            else:
+                self.end_game_button.background_color = [1, 0, 0]
+                self.new_game_flag = False
+                popup_content = Button(text=f'sum of numbers in all rows and columns\n '
+                                            f'is not equal {self.generete_rand}',
+                                       size=(400, 400))
+                popup = Popup(content=popup_content, auto_dismiss=False, title='nice', size_hint=(None, None),
+                              size=(400, 400))
+                popup_content.bind(on_press=popup.dismiss)
+                popup.open()
+        else:
+            screen_for_delete = self.main_layout.parent.manager.get_screen('game')
+
+            self.main_layout.parent.manager.current = 'settings'
+            self.main_layout.parent.manager.remove_widget(screen_for_delete)
+
+    def get_matrix(self):
+        import copy
+
+        self.generete_rand = self.size_of_gread * random.randint(self.size_of_gread, self.size_of_gread*random.randint(3, 6))
+        buttons = list()
+        Icount = 0
+        for i in range(self.size_of_gread):
+            tmp_mas = list()
+            random_arr = self.generete_rand
+            count = 0
+            if Icount == self.size_of_gread - 1:
+                for j in range(self.size_of_gread):
+                    column_sum = 0
+                    for k in buttons:
+                        column_sum += k[j]
+                    tmp_mas.append(self.generete_rand - column_sum)
+
+            else:
+                for j in range(self.size_of_gread):
+                    if self.size_of_gread - 1 == count:
+                        tmp_mas.append(random_arr)
+                    else:
+                        f = random.randint(self.size_of_gread, self.size_of_gread*2)
+                        random_arr -= f
+                        tmp_mas.append(f)
+                        count += 1
+
+            Icount += 1
+            buttons.append(tmp_mas)
+
+        new_arr = list()
+        for i in buttons:
+            for j in i:
+                new_arr.append(j)
+        random_arr = new_arr[:]
+        random.shuffle(random_arr)
+
+
+        for i in range(self.size_of_gread):
+            for j in range(self.size_of_gread):
+                buttons[i][j] = random_arr.pop()
+        # original_mass = copy.deepcopy(buttons)
+        # print('original')
+        # for i in original_mass:
+        #     print(i)
+        # print('shuffle')
+        # for i in buttons:
+        #     print(i)
+
+        for row in buttons:
+            row.append(sum(row))
+
+        random_arr = list()
+        for i in range(self.size_of_gread):
+            mysum = 0
+            for j in range(self.size_of_gread):
+                mysum += buttons[j][i]
+            random_arr.append(mysum)
+        random_arr.append(self.generete_rand)
+        buttons.append(random_arr)
+
+        return buttons
--- main.py
+++ main.py
@@ -1,21 +1,33 @@
-import datetime
-import random
 
-from kivy.animation import Animation
+
 from kivy.app import App
-from kivy.clock import Clock
-
-from kivy.core.window import Window
 from kivy.lang import Builder
-from kivy.properties import NumericProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.label import Label
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.textinput import TextInput
-from kivy.uix.slider import Slider
+from kivy.uix.screenmanager import ScreenManager
 
-MAIN_WINDOW_KV= """
+
+from screens import MenuScreen, SettingsScreen
+
+MAIN_WINDOW_KV="""
+
+#:import Factory kivy.factory.Factory
+
+<MyPopup@Popup>:
+    id: AboutPopup
+    auto_dismiss: False
+    title: 'About' 
+    orientation: "horizontal"
+    BoxLayout:
+        TextInput:
+            text:'Alternately swap the cubes until the sum of all horizontal and vertical ones is the same.\
+                For help, the required amount is indicated on the button at the bottom right.\
+                When the amount is the same everywhere, click on the button at the bottom right,\
+                see your result and proceed to the next level'
+            disabled: True 
+        Button:
+            text:'Close'
+            on_press: AboutPopup.dismiss()
+    
+
 <MenuScreen>:
     BoxLayout:
         orientation: "vertical"
@@ -23,15 +35,27 @@ MAIN_WINDOW_KV= """
             text: 'Start'
             on_press: root.manager.current = 'settings'
         Button:
-            text: 'Settings'
-            on_press: root.manager.current = 'game'
+            text: 'About'
+            on_release: Factory.MyPopup().open()
         Button:
             text: 'Quit'
-            on_press: app.stop() 
-        
+            on_press: app.stop()
+
 <SettingsScreen>:
     BoxLayout:
         orientation: "vertical"
+
+
+        Button:
+            text: 'return to game'
+            on_state:
+                if 'game' in root.manager.screen_names:\
+                    root.manager.current='game'
+                    
+        Button:
+            text: 'Back'
+            on_press: root.manager.current='menu'
+
         Label:
             text: 'Settings:'
             center_x: 0.5
@@ -40,15 +64,14 @@ MAIN_WINDOW_KV= """
             text: 'choose difficulty:'
             center_x: 0.5
             center_y: 0.5
-        
-        
+
         Slider:
             id: slider
-            min: 3
+            min: 2
             max: 15
             step: 1
             value_track: True
-            
+
         Label:
             text: str(slider.value)
 
@@ -61,192 +84,10 @@ MAIN_WINDOW_KV= """
 Builder.load_string(MAIN_WINDOW_KV)
 
 
-
-
-class MyGameApp(BoxLayout):
-    def __init__(self, size_of_gread):
-        super(MyGameApp, self).__init__()
-        self.size_of_gread= size_of_gread
-
-    def set_time(self,*args):
-        print(datetime.datetime.now()- self.start_time)
-        self.timer_label.text = str(datetime.datetime.now() - self.start_time)[0:7]
-
-
-
-    def build(self):
-        self.main_layout = BoxLayout(orientation="vertical")
-        self.start_time = datetime.datetime.now()
-        self.flag1 = False
-        self.flag2 = False
-        self.button_one = False
-        self.button_two = False
-        self.info_layout = BoxLayout(orientation="horizontal",size=(800, 50), size_hint=(None, None))
-        self.steps_label = Label(text=str(0))
-        Clock.schedule_interval(self.set_time,1)
-        self.timer_label = Label(text='0')
-        self.info_layout.add_widget(Label(text='Steps: '))
-        self.info_layout.add_widget(self.steps_label)
-        self.info_layout.add_widget(Label(text='Time: '))
-        self.info_layout.add_widget(self.timer_label)
-
-        self.main_layout.add_widget(self.info_layout)
-
-        self.all_number_layout = BoxLayout(orientation="vertical")
-        self.main_mass = self.get_matrix()
-
-        row_count=0
-        for row in self.main_mass:
-            h_layout = BoxLayout()
-            if row_count<self.size_of_gread:
-                lable_count=0
-                for label in row:
-                    lable_count+=1
-                    if lable_count<=self.size_of_gread:
-                        button = Button(text=str(label),pos_hint={"center_x": 0.5, "center_y": 0.5})
-                        button.bind(on_press=self.on_button_press)
-                    else:
-                        button = TextInput(text=str(label),pos_hint={"center_x": 0.5, "center_y": 0.5},disabled=True)
-
-                    h_layout.add_widget(button)
-
-            else:
-                for label in row:
-                    button = TextInput(text=str(label), pos_hint={"center_x": 0.5, "center_y": 0.5}, disabled=True)
-                    h_layout.add_widget(button)
-            self.all_number_layout.add_widget(h_layout)
-            row_count+=1
-
-        self.main_layout.add_widget(self.all_number_layout)
-        return self.main_layout
-
-
-    def on_button_press(self, button):
-
-        if self.flag1 == False:
-            self.flag1 = True
-            self.button_one = button
-            self.button_one.background_color=[0.216, 0.69, 0.22]
-        else:
-            self.flag2=True
-            self.button_two = button
-        if self.flag1 and self.flag2:
-            self.steps_label.text = str(int(self.steps_label.text)+1)
-            tmp_var = self.button_one.text
-            self.button_one.text=self.button_two.text
-            self.button_two.text = tmp_var
-            self.flag2 = False
-            self.flag1 = False
-            self.button_one.background_color = [1, 1, 1]
-            self.button_one.index = self.button_one.parent.children.index(self.button_one)
-            self.button_two.index = self.button_two.parent.children.index(self.button_two)
-
-            new_sum = 0
-            new_sum2=0
-
-            for i in range(1, self.size_of_gread + 1):
-                new_sum += int(self.button_one.parent.children[i].text)
-                new_sum2 += int(self.button_two.parent.children[i].text)
-            self.button_one.parent.children[0].text = str(new_sum)
-            self.button_two.parent.children[0].text = str(new_sum2)
-            new_sum_3=0
-            new_sum_4=0
-            first_flag = False
-            for i in self.all_number_layout.children:
-                if first_flag == False:
-                    first_flag = True
-                else:
-                    new_sum_3 += int(i.children[self.button_one.index].text)
-                    new_sum_4 += int(i.children[self.button_two.index].text)
-            self.all_number_layout.children[0].children[int(self.button_one.index)].text = str(new_sum_3)
-            self.all_number_layout.children[0].children[int(self.button_two.index)].text = str(new_sum_4)
-
-
-    def get_matrix(self):
-        import copy
-
-        generete_rand = self.size_of_gread * random.randint(self.size_of_gread, self.size_of_gread*random.randint(3,6))
-        buttons = list()
-        Icount = 0
-        for i in range(self.size_of_gread):
-            tmp_mas = list()
-            random_arr = generete_rand
-            count = 0
-            if Icount == self.size_of_gread - 1:
-                for j in range(self.size_of_gread):
-                    column_sum = 0
-                    for k in buttons:
-                        column_sum += k[j]
-                    tmp_mas.append(generete_rand - column_sum)
-
-            else:
-                for j in range(self.size_of_gread):
-                    if self.size_of_gread - 1 == count:
-                        tmp_mas.append(random_arr)
-                    else:
-                        f = random.randint(self.size_of_gread, self.size_of_gread*2)
-                        random_arr -= f
-                        tmp_mas.append(f)
-                        count += 1
-
-            Icount += 1
-            buttons.append(tmp_mas)
-
-
-        new_arr = list()
-        for i in buttons:
-            for j in i:
-                new_arr.append(j)
-        random_arr = new_arr[:]
-        random.shuffle(random_arr)
-        original_mass = copy.deepcopy(buttons)
-
-        for i in range(self.size_of_gread):
-            for j in range(self.size_of_gread):
-                buttons[i][j]=random_arr.pop()
-
-        print('original')
-        for i in original_mass:
-            print(i)
-        print('shuf')
-        for i in buttons:
-            print(i)
-
-        for row in buttons:
-            row.append(sum(row))
-
-        random_arr = list()
-        for i in range(self.size_of_gread):
-            mysum = 0
-            for j in range(self.size_of_gread):
-                mysum += buttons[j][i]
-            random_arr.append(mysum)
-        random_arr.append(generete_rand)
-        buttons.append(random_arr)
-
-        return buttons
-
-class MenuScreen(Screen):
-    pass
-
-class GameScreen(Screen):
-    def __init__(self,grid,**kwargs):
-        super(GameScreen, self).__init__(**kwargs)
-        layout = MyGameApp(grid).build()
-        self.add_widget(layout)
-
-
-class SettingsScreen(Screen):
-    def printMe(self):
-        self.parent.add_widget(GameScreen(grid=self.ids.slider.value,name='game'))
-        self.parent.current='game'
-
-
 class ExampleApp(App):
 
     def build(self):
         sm = ScreenManager()
-
         sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(SettingsScreen(name='settings'))
 
